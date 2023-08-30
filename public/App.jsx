0c5fd5e1f5ea43eb3d65bc3e7a2c325982cebeb8
chrisraygill <chrisraygill@gmail.com>
2023-08-30 23:52:49 +0000

    set up for hosting deploy

--- public/App.jsx
+++ public/App.jsx
@@ -2,6 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDxxfO7lpxc11DJcSLQmHQ2znr0O2g0SG8",
+  authDomain: "fir-day-poetry-app.firebaseapp.com",
+  projectId: "fir-day-poetry-app",
+  storageBucket: "fir-day-poetry-app.appspot.com",
+  messagingSenderId: "493852003640",
+  appId: "1:493852003640:web:d5eefcc58050a880f6c5cd",
+  measurementId: "G-3L6SST1EG1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   const [count, setCount] = useState(0)
